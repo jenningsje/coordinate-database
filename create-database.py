6773726ade5dea4d9f1d28ad138178f7e5a4a03d
jenningsje <53481699+jenningsje@users.noreply.github.com>
2021-11-10 08:22:08 -0600

    Add files via upload
    adds another column to the data frames that contain the coordinate data from the crystallographic information files.

---
+++ create-database.py
@@ -0,0 +1,24 @@
+import CifFile
+import pandas as pd
+import io
+from pathlib import Path
+import gzip
+
+directory = '/home/james/Desktop/'
+df = pd.DataFrame()
+
+for crystal_file in Path(directory).glob('*.cif.gz'):
+
+    if str(crystal_file) != "posix.DirEntry":
+
+        with gzip.open(crystal_file, 'rb') as ip:
+            cf = CifFile.ReadCif(ip)
+            all_data = cf.first_block()
+            atom_data = all_data.GetLoop("_atom_site.group_PDB")
+            df1 = pd.DataFrame.from_dict(atom_data)
+            df1=df1.assign(protein_id=lambda x : all_data)
+            print(df1)
+            df = df.append(df1, ignore_index=True)
+
+print(df)
+df.to_csv("df.csv")
