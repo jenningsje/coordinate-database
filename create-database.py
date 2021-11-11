fd9194157695b837cf9a71e0fc7cadd7bba3b69e
jenningsje <53481699+jenningsje@users.noreply.github.com>
2021-11-11 14:46:08 -0600

    Add files via upload
    figure out how to move files from a sub-directory to a different directory.

--- create-database.py
+++ create-database.py
@@ -8,31 +8,6 @@ import sys
 import os
 import shutil
 
-directory = '/home/james/Desktop/'
-directory = input("Enter the path of your file: ")
-df = pd.DataFrame()
-
-#description of program
-print("this program will loop through a directory containing gzipped files")
-print("and compress the data within the block _atom_site.group_PDB")
-print("in each gzipped file into a single csv file")
-print("within a single csv file")
-
-#computer gives user the option of moving all of their cif files
-#within subdirectories to a single directory of their choice
-print("if your gzipped cif files are all located within a single directory")
-print("it is recommended that you move them to a single directory of your choice?")
-print("would you like to do this? (y/n)")
-x = input()
-
-#begin if else statement for yes or no response
-if x == "yes":
-
-    print("please type in the directory containing all of your gizpped files")
-    input(source)
-    print("please type in the directory you wish to move your gzipped files to")
-
-    files = os.listdir(source)
 
 #Step 1 (yes response): move files to a directory of users choice
 for file in files:
@@ -42,65 +17,3 @@ for file in files:
 
     print("your files have just been moved")
     print("to the following directory:" + str(destination))
-
-    df_a = pd.DataFrame()
-
-#Step 2 (yes reponse): compress crystallographic information data
-#from the '_pdbx_database_status.entry_id' block into a single csv file
-for crystal_file in Path(destination).glob('*.cif.gz'):
-
-    if str(crystal_file).endswith('.cif.gz'):
-
-        with gzip.open(crystal_file, 'rb') as ip:
-
-            if '_atom_site.group_PDB' in ip:
-
-                cf = CifFile.ReadCif(ip)
-                all_data = cf.first_block()
-                atom_data = all_data.GetLoop("_atom_site.group_PDB")
-                df1 = pd.DataFrame.from_dict(atom_data)
-                df1=df1.assign(protein_id=lambda x : all_data['_pdbx_database_status.entry_id'])
-                print(df1)
-                df_a = df_a.append(df1, ignore_index=True)
-
-            else:
-    else:
-#end of code for the yes reponse
-
-#begin code for the no response
-else if x == "n":
-
-    directory = input("Enter the path of your crystallographic information files: ")
-    df = pd.DataFrame()
-
-    for crystal_file in Path(directory).glob('*.cif.gz'):
-
-    if str(crystal_file).endswith('.cif.gz'):
-for crystal_file in Path(directory).glob('*.cif.gz'):
-
-    if str(crystal_file) != "posix.DirEntry":
-        with gzip.open(crystal_file, 'rb') as ip:
-            cf = CifFile.ReadCif(ip)
-            all_data = cf.first_block()
-            df1=df1.assign(protein_id=lambda x : all_data['_pdbx_database_status.entry_id'])
-            print(df1)
-            df = df.append(df1, ignore_index=True)
-
-    print(df)
-    if df.empty
-        print("Dataframe is empty")
-        print("this means that either:")
-        print("1: you misspelled in the command prompt")
-        print("2: the directory does not exist")
-        print("3: the files are not crystallographic information files")
-        print("4: your crystallographic information files are empty")
-        df.to_csv("df.csv")
-    else
-        df.to_csv("df.csv")
-    break;
-
-    default
-    "please enter a yes or no response"
-}
-print(df)
-df.to_csv("df.csv")
